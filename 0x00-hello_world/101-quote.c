744009aaae8034880b475924694d5dcee59491e2
AhmedElSaeedTalat <ahmedelsaeed105@gmail.com>
2023-03-16 23:05:11 +0000

    quot

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -8,7 +8,9 @@
   */
 int main(void)
 {
-	write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", \
-	strlen("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n"));
+	char tx[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+
+
+	write(1, tx, strlen(tx));
 	return (1);
 }
