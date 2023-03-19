b62d3b9ba11e63db98d3e285b845cfb55371fc16
AhmedElSaeedTalat <ahmedelsaeed105@gmail.com>
2023-03-19 10:53:45 +0000

    combination

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+/**
+  * main - Entry point
+  *
+  * Return: Always 0 (Success)
+  */
+int main(void)
+{
+	int i, y;
+
+	for (i = 48; i <= 57; i++)
+	{
+		for (y = i + 1; y <= 57; y++)
+		{
+			putchar(i);
+			putchar(y);
+			putchar(44);
+			putchar(32);
+		};
+
+	};
+
+	putchar('\n');
+	return (0);
+}
