b30a7b875134c6c756e5248b3ae9433fb7645f57
AhmedElSaeedTalat <ahmedelsaeed105@gmail.com>
2023-03-22 00:20:56 +0000

    mod_natural

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -16,24 +16,33 @@ void print_to_98(int n)
 			if (i == 98)
 			{
 				printf(" %d", i);
+			} else if (i == n)
+			{
+				printf("%d,", i);
 			} else
 			{
 				printf(" %d,", i);
 			}
 		};
-	} else
+	} else if (n > 98)
 	{
 		for (i = n; i >= 98; i--)
 		{
 			if (i == 98)
 			{
 				printf(" %d", i);
+			} else if (i == n)
+			{
+				printf("%d", i);
 			} else
 			{
 				printf(" %d,", i);
 			}
 		}
-	}
+	} else
+	{
+		printf("%d", n);
+	};
 
 	putchar('\n');
 }
