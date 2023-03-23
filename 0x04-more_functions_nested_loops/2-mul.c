be67fafbd1102770cf9e7c074e70cb5620fee5bc
AhmedElSaeedTalat <ahmedelsaeed105@gmail.com>
2023-03-23 17:03:36 +0000

    multiply

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+  * muli - a function that multiplies two integers.
+  *
+  * @a: arg1
+  *
+  * @b: arg2
+  *
+  * Return: Always 0 (Success)
+  */
+int mul(int a, int b)
+{
+	int sum;
+
+	sum = a * b;
+	return (sum);
+}
