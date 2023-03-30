0ae722992f9fa8afe50f2f8d0daecc96d46c5653
AhmedElSaeedTalat <ahmedelsaeed105@gmail.com>
2023-03-30 11:54:03 +0000

    try

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <string.h>
 /**
   *  _strcmp - function that compares two strings
   *
@@ -12,9 +13,12 @@ int _strcmp(char *s1, char *s2)
 {
 	int i;
 
-	if (s1 == NULL || s2 == NULL)
+	if (s1 == NULL && s2 == NULL)
 	{
-		return (-15);
+		return (0);
+	} else if (s1 == NULL || s2 == NULL)
+	{
+		return (-1);
 	}
 
 	for (i = 0; *s1 == *s2; i++)
@@ -29,10 +33,10 @@ int _strcmp(char *s1, char *s2)
 
 	if (*s1 > *s2)
 	{
-		return (15);
+		return (1);
 	} else if (*s1 < *s2)
 	{
-		return (-15);
+		return (-1);
 	}
 
 	return (0);
