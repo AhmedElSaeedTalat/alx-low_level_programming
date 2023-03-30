b3a5dd7d533a0d70ad73487bc16877013e1363ab
AhmedElSaeedTalat <ahmedelsaeed105@gmail.com>
2023-03-30 19:52:56 +0000

    capitalize1

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,50 @@
+#include <stdio.h>
+/**
+  * check_flag - function that checks the flag
+  *
+  * @ch: letter sent to check if its flag
+  *
+  * Return: a value to confirm if the char was a flag
+  */
+int check_flag(int ch)
+{
+	int i;
+	int arr[30] = {44, 59, 46, 33, 63, 34, 41, 123, 125, 9, 10};
+
+	for (i = 0; i < 11; i++)
+	{
+		if (ch == arr[i])
+		{
+			return (1);
+		}
+	}
+
+	return (0);
+}
+/**
+  * cap_string - function that capitalizes all words
+  *
+  * @str: string to capitalize
+  *
+  * Return: a string to be capitalized
+  */
+char *cap_string(char *str)
+{
+	int i, flag;
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		if (flag == 1 && str[i] >= 'a' && str[i] <= 'z')
+		{
+			str[i] = str[i] - 32;
+			flag = 0;
+		} else if (flag == 1 && str[i] >= 65 && str[i] <= 90)
+		{
+			flag = 0;
+		}
+
+		flag = check_flag(str[i]);
+	}
+
+	return (str);
+}
