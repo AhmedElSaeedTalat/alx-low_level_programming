988ebc10018ddae064770512dead778782d152e5
AhmedElSaeedTalat <ahmedelsaeed105@gmail.com>
2023-03-30 01:19:33 +0000

    copy

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -19,7 +19,7 @@ char *_strncpy(char *dest, char *src, int n)
 	txt_src = src;
 	txt_dest = dest;
 
-	for (i = 0; i <= n && src[i] != '\0'; i++)
+	for (i = 0; i < n && src[i] != '\0'; i++)
 	{
 		*txt_dest = *txt_src;
 		txt_src++;
