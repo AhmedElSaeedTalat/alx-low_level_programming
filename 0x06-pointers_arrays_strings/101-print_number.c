7e64c63f0408f203272fa39e8d3da5bd6b2cc136
AhmedElSaeedTalat <ahmedelsaeed105@gmail.com>
2023-04-02 00:12:47 +0000

    print_int

---
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -0,0 +1,71 @@
+#include "main.h"
+/**
+  * check_len - function that checks how many digits in number
+  *
+  * @value: num to check
+  *
+  */
+int check_len(int value)
+{
+	int i, count_int;
+
+	count_int = 0;
+
+	for (i = 0; value != 0; i++)
+	{
+		value = value / 10;
+		count_int++;
+	}
+
+	return (count_int);
+}
+
+/**
+  * print_number - function that prints an integer
+  *
+  * @n: num received
+  *
+  */
+void print_number(int n)
+{
+	int i, count_int, value, tmp, count, num, y, negative;
+	char to_print;
+
+	value = n;
+	num = 1;
+
+	if (n < 0)
+	{
+		n =  -(n);
+		negative = 1;
+	}
+
+	if (n == 0)
+	{
+		_putchar(48);
+	}
+
+	count_int = check_len(value);
+	count = count_int;
+
+	for (i = 0; i < count_int; i++)
+	{
+		for (y = 1; y < count; y++)
+		{
+			num = num * 10;
+		};
+
+		tmp = n / num;
+		tmp = tmp % 10;
+
+		if (i == 0 && negative == 1)
+		{
+			_putchar(45);
+		}
+
+		to_print = tmp + '0';
+		_putchar(to_print);
+		num = 1;
+		count--;
+	}
+}
