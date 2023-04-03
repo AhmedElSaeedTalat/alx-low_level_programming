e9d3c66074e6524d68b30d39ada78f0b6ddfdc95
AhmedElSaeedTalat <ahmedelsaeed105@gmail.com>
2023-04-03 22:51:41 +0000

    find_strng

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -10,5 +10,29 @@
   */
 char *_strstr(char *haystack, char *needle)
 {
-	return (0);
+	int y, found;
+
+	found = 0;
+	for (; *haystack != '\0';)
+	{
+		for (y = 0; needle[y] != '\0'; y++)
+		{
+			if (*haystack == needle[y])
+			{
+				if (*++haystack == needle[y + 1])
+				{
+					haystack--;
+					found = 1;
+					return (haystack);
+				}
+			}
+		}
+		haystack++;
+	}
+
+	if (found == 0)
+	{
+		return (NULL);
+	}
+	return (haystack);
 }
