ecc7eb3efe89a55b520e77e6412add387ad1da72
AhmedElSaeedTalat <ahmedelsaeed105@gmail.com>
2023-04-03 20:46:26 +0000

    strchr

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -10,5 +10,31 @@
   */
 unsigned int _strspn(char *s, char *accept)
 {
-	return (0);
+	int i, y, count, found;
+
+	count = 0;
+	found = 0;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		if (i > 0 && found == 1)
+		{
+			found = 0;
+		}
+
+		for (y = 0; accept[y] != '\0'; y++)
+		{
+			if (s[i] == accept[y])
+			{
+				count++;
+				found = 1;
+			}
+		}
+
+		if (found == 0)
+		{
+			return (count);
+		}
+	}
+	return (count);
 }
