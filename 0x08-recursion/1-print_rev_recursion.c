d4ded6bcba70b9bd58c9d2d437e0c4bce76185b7
AhmedElSaeedTalat <ahmedelsaeed105@gmail.com>
2023-04-05 19:47:50 +0000

    reverse

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+  * _print_rev_recursion - function that prints a string in reverse
+  *
+  * @s: string received
+  */
+void _print_rev_recursion(char *s)
+{
+	char *first_address;
+
+	first_address = s;
+	s++;
+	if (*s == '\0')
+	{
+		s--;
+		_putchar(*s);
+		return;
+	}
+
+	_print_rev_recursion(s);
+	s--;
+	_putchar(*s);
+	if (*s == *first_address)
+	{
+		return;
+	}
+
+}
