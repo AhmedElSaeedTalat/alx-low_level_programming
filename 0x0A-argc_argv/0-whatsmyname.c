40b2c962734bdd65a8685393836eb02a9841e742
AhmedElSaeedTalat <ahmedelsaeed105@gmail.com>
2023-04-07 21:52:22 +0000

    first argc

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+  * main - prints its name, followed by a new line
+  *
+  * @argc: size of array
+  *
+  * @argv: array of strings
+  *
+  * Return: Always 0 (Success)
+  */
+int main(int argc, char *argv[])
+{
+	(void) argc;
+	printf("%s\n", argv[0]);
+	return (0);
+}
