8fe8f539d47351e7d5084d0caff70a3b6ea14086
AhmedElSaeedTalat <ahmedelsaeed105@gmail.com>
2023-04-11 18:48:07 +0000

    task 1 copy string

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include "main.h"
+#include <stdlib.h>
+/**
+  * _strdup - contains a copy of the string given as a parameter.
+  *
+  * @str: received string to copy
+  *
+  * Return: pointer to copied string in newly allocated memory
+  */
+char *_strdup(char *str)
+{
+	int i, count;
+	char *s;
+
+	count = 0;
+	if (str == NULL)
+		return (NULL);
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		count++;
+	}
+
+	s = malloc(sizeof(char) * count);
+	if (s == NULL)
+		return (NULL);
+
+	for (i = 0; i < count; i++)
+	{
+		s[i] = str[i];
+	}
+
+	return (s);
+}
