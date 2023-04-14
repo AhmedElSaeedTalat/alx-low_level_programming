af891ff3afec9faa2cd3dc1be9a5d76c881077ec
AhmedElSaeedTalat <ahmedelsaeed105@gmail.com>
2023-04-14 00:49:27 +0000

    free ptr

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -17,18 +17,18 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 {
 	unsigned int *str, *new_ptr, i;
 
-	if (new_size == 0 && ptr != NULL)
-	{
-		free(ptr);
-		return (NULL);
-	}
-
 	if (ptr == NULL)
 	{
 		new_ptr = malloc(new_size);
 		if (new_ptr == NULL)
 			return (NULL);
 		return (new_ptr);
+	}
+
+	if (new_size == 0 && ptr != NULL)
+	{
+		free(ptr);
+		return (NULL);
 	}
 
 	if (new_size == old_size)
