36cff6f58fb922a1d431e578b6407ae2bf05c458
AhmedElSaeedTalat <ahmedelsaeed105@gmail.com>
2023-04-17 18:11:09 +0000

    create new dog2

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -16,15 +16,24 @@ dog_t *new_dog(char *name, float age, char *owner)
 {
 	dog_t *new_dog;
 
-	if (name == NULL || owner == NULL)
-		return (NULL);
-
 	new_dog = malloc(sizeof(dog_t));
 	if (new_dog == NULL)
 		return (NULL);
 
 	new_dog->name = name;
+	if (new_dog->name == NULL)
+	{
+		free(new_dog);
+		return (NULL);
+	}
+
 	new_dog->age = age;
 	new_dog->owner = owner;
+	if (new_dog->owner == NULL)
+	{
+		free(new_dog);
+		return (NULL);
+	}
+
 	return (new_dog);
 }
