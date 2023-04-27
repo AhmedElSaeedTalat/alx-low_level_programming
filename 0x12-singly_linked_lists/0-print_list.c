bbfd55b7aa3dc328be3b9e26b2325d1c3329b656
AhmedElSaeedTalat <ahmedelsaeed105@gmail.com>
2023-04-27 15:21:05 +0000

    task 0

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -16,12 +16,12 @@ size_t print_list(const list_t *h)
 
 	ptr = h;
 	count = 0;
-	while (ptr != NULL)
+	while (ptr)
 	{
 		if (ptr->str == NULL)
 			printf("[0] (nil)\n");
 		else
-			printf("[ %d ] %s\n", ptr->len, ptr->str);
+			printf("[%d] %s\n", ptr->len, ptr->str);
 		ptr = ptr->next;
 		count++;
 	}
