c772573395abe359a34c5cae09f5e7a1920eae96
AhmedElSaeedTalat <ahmedelsaeed105@gmail.com>
2023-04-27 16:13:46 +0000

    task 2.1

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -1,5 +1,26 @@
 #include "lists.h"
 /**
+  * s_len - count length
+  *
+  * @str: str received
+  *
+  * Return: count
+  */
+int s_len(const char *str)
+{
+	int count;
+	const char *ptr = str;
+
+	count = 0;
+
+	while (*ptr != '\0')
+	{
+		count++;
+		ptr++;
+	}
+	return (count);
+}
+/**
   * add_node - adds a new node at the beginning
   *
   * @head: current head of linked list
@@ -15,7 +36,7 @@ list_t *add_node(list_t **head, const char *str)
 	if (new == NULL)
 		return (NULL);
 	new->str = strdup(str);
-	new->len = strlen(str);
+	new->len = s_len(str);
 	new->next = *head;
 	*head = new;
 	return (new);
