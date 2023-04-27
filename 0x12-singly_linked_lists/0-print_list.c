8aefcc1433ecf4e154fe9d9a28562acb237dea56
AhmedElSaeedTalat <ahmedelsaeed105@gmail.com>
2023-04-27 15:14:43 +0000

    task 0

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "lists.h"
+/**
+  * print_list -  prints all the elements
+  *
+  * @h: list received
+  *
+  * Return: count elements
+  */
+size_t print_list(const list_t *h)
+{
+	int count;
+	const list_t *ptr;
+
+	ptr = h;
+	count = 0;
+	while (ptr != NULL)
+	{
+		if (ptr->str == NULL)
+			printf("[ 0 ] (nil)\n");
+		else
+			printf("[ %d ] %s\n", ptr->len, ptr->str);
+		ptr = ptr->next;
+		count++;
+	}
+
+	return (count);
+}
