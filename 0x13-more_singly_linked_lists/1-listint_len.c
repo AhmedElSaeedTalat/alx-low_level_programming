348569ec8e1d7100894e1cf77aefca9929650426
AhmedElSaeedTalat <ahmedelsaeed105@gmail.com>
2023-04-29 16:38:49 +0000

    modify betty

--- 0x13-more_singly_linked_lists/1-listint_len.c
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -1,4 +1,4 @@
-#include <stdio.h>
+#include "lists.h"
 /**
   * listint_len -  returns the number of elements
   *
@@ -8,5 +8,14 @@
   */
 size_t listint_len(const listint_t *h)
 {
-	return (0);
+	int count;
+
+	count = 0;
+	while (h != NULL)
+	{
+		h = h->next;
+		count++;
+	}
+
+	return (count);
 }
