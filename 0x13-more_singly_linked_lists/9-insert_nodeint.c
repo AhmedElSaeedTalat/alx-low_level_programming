c42432dd7b5b8bbe2b2ded61e590382b7ade9b72
AhmedElSaeedTalat <ahmedelsaeed105@gmail.com>
2023-04-30 00:51:39 +0000

    task 9

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -41,7 +41,10 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		curr = curr->next;
 		i++;
 	}
-	if (idx >= i)
+	if (idx > i)
+	{
+		free(new);
 		return (NULL);
+	}
 	return (new);
 }
