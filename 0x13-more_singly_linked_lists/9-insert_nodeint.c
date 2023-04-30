d04f21e618ce222b03bfae6f840eea4a89e3761a
AhmedElSaeedTalat <ahmedelsaeed105@gmail.com>
2023-04-30 03:36:39 +0000

    task 9

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -37,7 +37,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	unsigned int i, length;
 
 	length = get_len(head);
-	if ((*head == NULL && idx != 0) || idx >= length)
+	if ((*head == NULL && idx != 0) || idx > length)
 		return (NULL);
 	new = malloc(sizeof(listint_t));
 	if (new == NULL)
