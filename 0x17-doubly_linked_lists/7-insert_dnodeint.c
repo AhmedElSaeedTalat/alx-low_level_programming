e79e0faeb47c09dc01dd60fa4c7cd5d3ff5ee63a
AhmedElSaeedTalat <ahmedelsaeed105@gmail.com>
2023-06-15 11:34:46 +0000

    t7-update

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -18,7 +18,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	if (new_node == NULL)
 		return (NULL);
 	new_node->n = n;
-	if (h == NULL)
+	if (*h == NULL)
 	{
 		new_node->next = NULL;
 		new_node->prev = NULL;
