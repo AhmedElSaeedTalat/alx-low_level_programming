62d351f7b37e15c55aef9dcb268cb4b98045c092
AhmedElSaeedTalat <ahmedelsaeed105@gmail.com>
2023-07-07 11:24:02 +0000

    t3

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -117,7 +117,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	unsigned long int index, val;
 	hash_node_t *item;
 
-	if (key == NULL)
+	if (key == NULL || ht == NULL || value == NULL)
 		return (0);
 	index = key_index((const unsigned char *) key, ht->size);
 	if (ht->array[index] == NULL)
