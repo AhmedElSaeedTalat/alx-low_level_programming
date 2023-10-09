581ffd79f51be377edf46b2e8ec5f691a664fc44
AhmedElSaeedTalat <ahmedelsaeed105@gmail.com>
2023-10-09 09:16:06 +0000

    linear search t0

---
+++ 0x1E-search_algorithms/0-linear.c
@@ -0,0 +1,26 @@
+#include "search_algos.h"
+/**
+ * linear_search - search linaer algorithm
+ *
+ * @array: array passed
+ * @size: size of array
+ * @value: value to search
+ *
+ * Return: index of value or -1 if not found
+ */
+int linear_search(int *array, size_t size, int value)
+{
+	size_t i;
+
+	if (array == NULL)
+		return (-1);
+	for (i = 0; i < size; i++)
+	{
+		printf("Value checked array[0] = [%d]\n", array[i]);
+		if (array[i] == value)
+			break;
+	}
+	if (i == size)
+		return (-1);
+	return (i);
+}
