aa57718db2795ea377e4aa12b7b7e93355b6098e
AhmedElSaeedTalat <ahmedelsaeed105@gmail.com>
2023-10-10 07:56:34 +0000

    t100

---
+++ 0x1E-search_algorithms/100-jump.c
@@ -0,0 +1,52 @@
+#include "search_algos.h"
+#include <math.h>
+/**
+ * print - print value compared to
+ *
+ * @array: pointer to array passed
+ * @i: index to value to print
+ */
+void print(int *array, int i)
+{
+	printf("Value checked array[%d] = [%d]\n", i, array[i]);
+}
+/**
+ * jump_search - jump search algorith
+ *
+ * @array: pointer to array passed
+ * @size: size of array passed
+ * @value: to search
+ *
+ * Return: -1 on failure, index on success
+ */
+int jump_search(int *array, size_t size, int value)
+{
+	int jump, l, h, i;
+
+	if (array == NULL)
+		return (-1);
+	jump = sqrt((int) size);
+
+	for (l = 0, h = l + jump; h < (int) size; l += jump, h += jump)
+	{
+		print(array, l);
+		if (value > array[l] && value > array[h])
+		{
+			continue;
+		}
+		else if (value >= array[l] && value <= array[h])
+		{
+			printf("Value found between indexes [%d] and [%d]\n", l, h);
+			for (i = l; i <= h; i++)
+			{
+				print(array, i);
+				if (value == array[i])
+					return (i);
+			}
+		}
+	}
+	print(array, l);
+	printf("Value found between indexes [%d] and [%d]\n", l, h);
+	print(array, l);
+	return (-1);
+}
